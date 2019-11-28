81899456d3cef60a7bc91cb67bbc47e3a3df887a
lommyti <thomasli2020@u.northwestern.edu>
2019-11-27 19:41:24 -0600

    fixed pic names

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Leaf from './Leaf'
-import RoundLeaf from './RoundLeaf'
-import Cactus from './Cactus'
-import Rock from './Rock'
-import Typing from 'react-typing-animation';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Leaf from "./Leaf";
+import RoundLeaf from "./RoundLeaf";
+import Cactus from "./Cactus";
+import Rock from "./Rock";
+import Typing from "react-typing-animation";
 
 class App extends Component {
   constructor(props) {
@@ -13,103 +13,100 @@ class App extends Component {
     this.state = {
       leaves: [],
       leaves2: [],
-      roundleaves:[],
-      rocks:[],
-      cactus:[],
-      start: 'false',
-    }
+      roundleaves: [],
+      rocks: [],
+      cactus: [],
+      start: "false"
+    };
   }
 
-  setSandbox(){
-    this.makeLeaves()
-    this.makeRoundLeaves()
-    this.makeRocks()
-    this.makeCactus()
+  setSandbox() {
+    this.makeLeaves();
+    this.makeRoundLeaves();
+    this.makeRocks();
+    this.makeCactus();
   }
 
-  makeLeaves(){
-    var temp = []
-    var temp2 = []
-    this.setState({leaves: temp})
-    this.setState({leaves2: temp2})
-    for(let i = 0; i < 40; i++) {
-      if(i > 15 || i % 2 != 0){
-        temp.push(<Leaf key={i} leafNum={i} />)
+  makeLeaves() {
+    var temp = [];
+    var temp2 = [];
+    this.setState({ leaves: temp });
+    this.setState({ leaves2: temp2 });
+    for (let i = 0; i < 40; i++) {
+      if (i > 15 || i % 2 != 0) {
+        temp.push(<Leaf key={i} leafNum={i} />);
       }
     }
-    this.setState({leaves: temp})
-    for(let i = 0; i < 50; i++) {
-      if(i > 15 || i % 2 != 0){
-        temp2.push(<Leaf key={i} leafNum={i} />)
+    this.setState({ leaves: temp });
+    for (let i = 0; i < 50; i++) {
+      if (i > 15 || i % 2 != 0) {
+        temp2.push(<Leaf key={i} leafNum={i} />);
       }
     }
-    this.setState({leaves2: temp2})
-    this.setState({start:'true'})
+    this.setState({ leaves2: temp2 });
+    this.setState({ start: "true" });
   }
 
-  makeRoundLeaves(){
-    var temp = []
-    this.setState({roundleaves: temp})
-    for(let i = 0; i < 35; i++) {
-      if(i > 15 || i % 2 != 0){
-        temp.push(<RoundLeaf key={i} leafNum={i} />)
+  makeRoundLeaves() {
+    var temp = [];
+    this.setState({ roundleaves: temp });
+    for (let i = 0; i < 35; i++) {
+      if (i > 15 || i % 2 != 0) {
+        temp.push(<RoundLeaf key={i} leafNum={i} />);
       }
     }
-    this.setState({roundleaves: temp})
+    this.setState({ roundleaves: temp });
   }
 
-  makeCactus(){
-    var temp = []
-    this.setState({cactus: temp})
-    for(let i = 0; i < 40; i++) {
-      if(i > 15 || i % 2 != 0){
-        temp.push(<Cactus key={i} leafNum={i} />)
+  makeCactus() {
+    var temp = [];
+    this.setState({ cactus: temp });
+    for (let i = 0; i < 40; i++) {
+      if (i > 15 || i % 2 != 0) {
+        temp.push(<Cactus key={i} leafNum={i} />);
       }
     }
-    this.setState({cactus: temp})
+    this.setState({ cactus: temp });
   }
 
-  makeRocks(){
-    var temp = []
-    this.setState({rocks: temp})
-    for(let i = 0; i < 3500; i++) {
-      temp.push(<Rock key={i} rockNum={(i%6)+7} />)
+  makeRocks() {
+    var temp = [];
+    this.setState({ rocks: temp });
+    for (let i = 0; i < 500; i++) {
+      temp.push(<Rock key={i} rockNum={(i % 6) + 7} />);
     }
-    this.setState({rocks: temp})
+    this.setState({ rocks: temp });
   }
 
-  renderButton(){
-    if (this.state.start == 'false'){
-      return(
+  renderButton() {
+    if (this.state.start == "false") {
+      return (
         <Typing loop="true" speed={80}>
-                <button onClick={()=>this.setSandbox()}> YOU COME ACROSS A BARREN AND SANDY PLAIN. CLICK TO GROW THIS GARDEN  </button>
-                <Typing.Delay ms={10000} />
-        </Typing>)
-    }
-    else{
-      return
-    }
-  }
-
-  renderFinished(){
-    if (this.state.start == 'false'){
-      return
-    }
-    else{
-      return(
-        <button> WOULD YOU LOOK AT THAT! IT CAME TO LIFE! </button>
-      )
+          <button onClick={() => this.setSandbox()}>
+            {" "}
+            YOU COME ACROSS A BARREN AND SANDY PLAIN. CLICK TO GROW THIS GARDEN{" "}
+          </button>
+          <Typing.Delay ms={10000} />
+        </Typing>
+      );
+    } else {
+      return;
     }
   }
 
-  renderCactusBase(){
-    if (this.state.start == 'false'){
-      return
+  renderFinished() {
+    if (this.state.start == "false") {
+      return;
+    } else {
+      return <button> WOULD YOU LOOK AT THAT! IT CAME TO LIFE! </button>;
     }
-    else{
-      return(
-        <div className="cactus-base"></div>
-      )
+  }
+
+  renderCactusBase() {
+    if (this.state.start == "false") {
+      return;
+    } else {
+      return <div className="cactus-base" />;
     }
   }
 
@@ -118,23 +115,13 @@ class App extends Component {
       <div className="App">
         <h3>SUCCULENT SANDBOX</h3>
         <div className="sandbox">
-          <div className="rocks">
-            {this.state.rocks}
-          </div>
-          <div className="leaves">
-            {this.state.leaves}
-          </div>
-          <div className="leaves2">
-            {this.state.leaves2}
-          </div>
-          <div className="roundleaves">
-            {this.state.roundleaves}
-          </div>
+          <div className="rocks">{this.state.rocks}</div>
+          <div className="leaves">{this.state.leaves}</div>
+          <div className="leaves2">{this.state.leaves2}</div>
+          <div className="roundleaves">{this.state.roundleaves}</div>
           <div className="cacti">
             {this.renderCactusBase()}
-            <div className="cactus-inside-container">
-              {this.state.cactus}
-            </div>
+            <div className="cactus-inside-container">{this.state.cactus}</div>
           </div>
         </div>
         {this.renderButton()}
--- src/Leaf.js
+++ src/Leaf.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class Leaf extends Component {
   render() {
     const adjustment = {
       paddingTop: Math.min(Math.pow(this.props.leafNum, 1.2), 35) + "px",
       width: Math.max(Math.pow(this.props.leafNum, 0.85), 15) + "px",
-      transform: "rotate(" + ((8*Math.random())+ (this.props.leafNum * 34))  + "deg) translate(0," + Math.pow(this.props.leafNum, 1.2)/2 + "px)",
+      transform:
+        "rotate(" +
+        (8 * Math.random() + this.props.leafNum * 34) +
+        "deg) translate(0," +
+        Math.pow(this.props.leafNum, 1.2) / 2 +
+        "px)",
       zIndex: -this.props.leafNum,
-      animationDelay: this.props.leafNum/10 + "s"
-    }
+      animationDelay: this.props.leafNum / 10 + "s"
+    };
     const tipWidth = {
       width: Math.max(Math.pow(this.props.leafNum, 0.85), 15) + "px",
-      opacity: Math.min(this.props.leafNum-5, 1),
-    }
+      opacity: Math.min(this.props.leafNum - 5, 1)
+    };
     return (
       <div className="leaf-container">
-      <div className="leaf" style={adjustment}>
-      <img src="/img/pink%20tip.png" style={tipWidth} />
-      </div>
+        <div className="leaf" style={adjustment}>
+          <img src="/img/pinktip.png" style={tipWidth} />
+        </div>
       </div>
     );
   }
