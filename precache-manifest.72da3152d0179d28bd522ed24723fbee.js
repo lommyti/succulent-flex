311bfcbc4410c4526bcee387384f1a5eef8b5659
lommyti <thomasli2020@u.northwestern.edu>
2019-11-27 19:38:33 -0600

    Updates

---
+++ precache-manifest.72da3152d0179d28bd522ed24723fbee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/succulent-flex/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e667bcac0cc6b8791b37",
+    "url": "/succulent-flex/static/js/runtime~main.e667bcac.js"
+  },
+  {
+    "revision": "beeb6ff8b55fc9ab44ef",
+    "url": "/succulent-flex/static/js/main.beeb6ff8.chunk.js"
+  },
+  {
+    "revision": "afbba25b570244ba5169",
+    "url": "/succulent-flex/static/js/1.afbba25b.chunk.js"
+  },
+  {
+    "revision": "beeb6ff8b55fc9ab44ef",
+    "url": "/succulent-flex/static/css/main.8d7c6300.chunk.css"
+  },
+  {
+    "revision": "1c860e67dfe6c38dfc75e13a89f0bb98",
+    "url": "/succulent-flex/index.html"
+  }
+];
